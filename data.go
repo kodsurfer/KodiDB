8e782516aa07577f25cb13342eb3d5982c23f132
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2022-10-14 00:58:40 +0300

    Create data.go

---
+++ data.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type data struct {
+	file *os.File
+}
+
+func NewData(path string) (*data, error) {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return &data{
+		file: file,
+	}, nil
+
+}
+
+func (d *data) close() error {
+	if d.file != nil {
+		err := d.file.Close()
+		if err != nil {
+			return fmt.Errorf("close file error: %s", err)
+		}
+		d.file = nil
+	}
+	return nil
+}
