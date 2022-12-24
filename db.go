8f536fea1df938c2037bd4e5dcebe97f383735e6
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2022-12-24 16:56:41 +0300

    Update db.go

--- db.go
+++ db.go
@@ -2,6 +2,17 @@ package main
 
 import "sync"
 
+type keyLocks struct {
+  sync.RWMutex
+  keys map[uint64]struct{}
+}
+
+func (k *keyLocks) add(key uint64) {
+  k.Lock()
+  defer k.Unlock()
+  k.keys[key] = struct{}{}
+}
+
 type DB struct {
   lock sync.RWMutex
 }
