a3d51841c25fd08d065ed4bf0a7a9a3d18714a75
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2022-12-24 17:04:43 +0300

    Update db.go

--- db.go
+++ db.go
@@ -13,6 +13,13 @@ func (k *keyLocks) add(key uint64) {
   k.keys[key] = struct{}{}
 }
 
+func (k *keyLocks) exist(key uint64) bool {
+	k.RLock()
+	defer k.RUnlock()
+	_, ok := k.keys[key]
+	return ok
+}
+
 type DB struct {
   lock sync.RWMutex
 }
